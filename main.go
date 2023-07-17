66cbb0dc62a6086602913d4ff06d70c9c8d08b8f
soso555BoBs <soso555bobs@gmail.com>
2023-07-17 16:48:56 +0900

    add ecspresso

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World! Welcome Go App!")
 }
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
